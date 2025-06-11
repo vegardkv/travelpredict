fe40a2f7c379ed94cb5beaa9b1048090b9b5efdc
vegardkv <vkvernelv@gmail.com>
2025-06-11 22:04:51 +0200

    Add another DST fix

--- download-data.py
+++ download-data.py
@@ -1,18 +1,21 @@
 from entur_collector import run_scheduled_downloads
 from entur_collector.dataanalysis.convertdata import process_raw_data
 import datetime
+import pytz
 
 
 if __name__ == "__main__":
     current_time = datetime.datetime.now().time()
+    now_in_norway = datetime.datetime.now(pytz.timezone("Europe/Oslo"))
+    dst = now_in_norway.dst()
     if current_time.hour >= 12:
         # This is a hack to make the script run in the afternoon as well.
         # This is convenient for testing purposes.
-        START_TIME = "15:00"
-        END_TIME = "17:00"
+        START_TIME = f"{15 - dst.seconds // 3600}:00"
+        END_TIME = f"{17 - dst.seconds // 3600}:00"
     else:
-        START_TIME = "5:00"
-        END_TIME = "9:00"
+        START_TIME = f"{5 - dst.seconds // 3600}:00"
+        END_TIME = f"{9 - dst.seconds // 3600}:00"
 
     # Download interval in seconds (e.g., 300 = 5 minutes)
     INTERVAL = 15
